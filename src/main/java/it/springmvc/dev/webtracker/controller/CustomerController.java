f5932505f33f4188c06304373cb5fe16d2340e30
Ramai Alejandria <vyasadev931@gmail.com>
2022-12-10 21:11:01 +0100

    Initial commit

---
+++ src/main/java/it/springmvc/dev/webtracker/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package it.springmvc.dev.webtracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/customer")
+@Controller
+public class CustomerController {
+
+    @RequestMapping("/list")
+    public String listCustomers(Model model) {
+        return "list-customers";
+    }
+}
---
+++ src/test/java/it/springmvc/dev/webtracker/WebCustomerTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.springmvc.dev.webtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCustomerTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
